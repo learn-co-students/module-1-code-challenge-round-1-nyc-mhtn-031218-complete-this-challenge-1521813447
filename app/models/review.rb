5bce9115d9c8a658dc0b892f94b582b1ac807b60
rogercodes1 <roger.ucsb@gmail.com>
2018-03-23 11:12:03 -0400

    Code Challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,38 @@
 class Review
-  
+
+  attr_accessor :review
+  ALL = []
+
+  def initialize(review)
+    @review = review
+
+    ALL << self
+  end
+
+
+  def customer(name)
+    binding.pry
+    Customer.find_by_name(name)
+
+  end
+
+  def restaurant
+
+
+  end
+
+  def self.all
+    ALL
+  end 
+
+
 end
 
+
+
+# Review.all
+# returns all of the reviews
+# Review#customer
+# returns the customer for that given review
+# Review#restaurant
+# returns the restaurant for that given review
